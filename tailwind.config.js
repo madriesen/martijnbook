4037a366de3248324bdddc2a31ae669e03154ac0
madriesen <martijn.driesen5@hotmail.com>
2021-07-27 12:48:51 +0200

    add netlify configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   prefix: "",
-  // purge: {
-  //   enabled: true,
-  //   content: ["./src/**/*.{html,ts}"],
-  // },
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,ts}"],
+  },
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
